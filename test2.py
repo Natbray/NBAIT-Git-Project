91a0c3e34c74370490a978dde041951cfd684646
Natbray <86750899+Natbray@users.noreply.github.com>
2023-05-10 10:17:12 +0930

    Initial upload
    adding the raw code

---
+++ test2.py
@@ -0,0 +1,5 @@
+var1 = 1
+if var1 == 1:
+    print('hello world')
+else:
+    print('hallo earth')
---
+++ test3.py
@@ -0,0 +1,5 @@
+var1 = 1
+while var1 < 10:
+    print('hallo earth')
+    var1 + 1
+#this will result in an infinite loop, please fix it    
